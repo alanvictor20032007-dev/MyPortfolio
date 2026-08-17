12de76b3406d374bacdecfc21fd0baef83dfcbb5
Alan Victor <alanvictor20032007@gmail.com>
2026-08-17 11:37:33 +0530

    day 10

--- script.js
+++ script.js
@@ -13,3 +13,34 @@ let hero=document.querySelector(".hero-section h1")
 if(hero){
     hero.textContent=`${getGreeting()}, I am Alan Victor.`
 }
+let menuToggle=document.querySelector(".menu-toggle");
+let navLinks=document.querySelector(".nav-links");
+menuToggle.addEventListener("click",()=>{
+    navLinks.classList.toggle('open');
+    menuToggle.setAttribute("aria-expanded")
+    navLinks.classList.contains("open")
+})
+
+let header=document.querySelector(".class-header")
+window.addEventListener()
+
+
+
+let sections=document.querySelectorAll('sections[id]');
+let navitems=document.querySelectorAll(".nav-links a")
+
+window.addEventListener("scroll",()=>{
+    let current="";
+    sections.forEach(section => {
+        if(window.scrollY>= section.offsetTop-100){
+            current=section.getAttribute("id")
+        }   
+    });
+})
+
+navitems.forEach(link=>{
+    link.classList.remove("active");
+    if(link.getAttribute('href')==`#${current}`){
+        link.classList.add('active')
+    }
+})
